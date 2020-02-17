341d827f0a8052569b27855f7a2a67df881afb78
solidstatejake <jberg3@binghamton.edu>
2020-02-17 13:19:48 -0500

    Makefile and CMake  commit

---
+++ CMakeFiles/ProgrammingAssignment1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProgrammingAssignment1.dir/main.cpp.o"
+  "ProgrammingAssignment1"
+  "ProgrammingAssignment1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProgrammingAssignment1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
